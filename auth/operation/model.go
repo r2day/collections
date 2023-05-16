517af8e9226196019aaeaf4add33e62641cbd879
dropthemasquerade <dropthemasquerade@gmail.com>
2023-05-16 10:14:22 +0800

    operations logs

---
+++ auth/operation/model.go
@@ -0,0 +1,52 @@
+package operation
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+const (
+	// CollectionNamePrefix 数据库表前缀
+	// 可以根据具体业务的需要进行定义
+	// 例如: sys_, scm_, customer_, order_ 等
+	collectionNamePrefix = "auth_"
+	// CollectionNameSuffix 后缀
+	// 例如, _log, _config, _flow,
+	collectionNameSuffix = "_log"
+	// 这个需要用户根据具体业务完成设定
+	modelName = "operation"
+)
+
+// Model 模型
+type Model struct {
+	// 基本的数据库模型字段，一般情况所有model都应该包含如下字段
+	// 创建时（用户上传的数据为空，所以默认可以不传该值)
+	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	// 商户号
+	MerchantID string `json:"merchant_id" bson:"merchant_id"`
+	// 创建者
+	AccountID string `json:"account_id" bson:"account_id"`
+	// 创建时间
+	CreatedAt string `json:"created_at" bson:"created_at"`
+	// 更新时间
+	UpdatedAt string `json:"updated_at" bson:"updated_at"`
+	// 状态
+	Status bool `json:"status"`
+	// 根据角色的最低级别写入
+	AccessLevel uint `json:"access_level" bson:"access_level"`
+
+	// 用户根据业务需求定义的字段
+	// 客户IP
+	ClientIP string `json:"client_ip" bson:"client_ip"`
+	// 远程IP
+	RemoteIP string `json:"remote_ip"  bson:"remote_ip"`
+	// 路径
+	FullPath string `json:"full_path"  bson:"full_path"`
+	// 请求方法/操作
+	Method string `json:"method"  bson:"method"`
+	// 相应代码
+	RespCode int `json:"resp_code"  bson:"resp_code"`
+	// 操作对象id
+	TargetID string `json:"target_id"  bson:"target_id"`
+	// 操作行为
+	Operation string `json:"operation"  bson:"operation"`
+}
--- go.mod
+++ go.mod
@@ -4,14 +4,16 @@ go 1.20
 
 require (
 	github.com/r2day/base v1.6.7
-	github.com/r2day/db v0.3.2
+	github.com/r2day/db v0.3.5
 	github.com/r2day/rest v0.2.8
 	github.com/sirupsen/logrus v1.9.0
 	go.mongodb.org/mongo-driver v1.11.3
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -28,6 +30,7 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/redis/go-redis/v9 v9.0.3 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
